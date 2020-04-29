9740b7fb7eead62208c5741a93fcd732a0b3da96
vipingitac <vipin.roby@gmail.com>
2020-04-29 07:53:54 +0000

    updated

--- main.c
+++ main.c
@@ -5,25 +5,16 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 feature1()
 {
   //Sample Code for Feature 1
-=======
 feaature2()
 {
   //Sample Code for Feature 2
 }
->>>>>>> cf1dc6b42a1234b60ad02013916fd94f07b83932
-=======
 Initial Security()
 {
  //Sample Code
->>>>>>> f71ccdccb44d9637abf24e4a39f5976da5baf587
 }
-
->>>>>>> f71ccdccb44d9637abf24e4a39f5976da5baf587
-
 
 }
